97013e44a9920144c7ee8b6d17dde46e849d2863
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-12-16 12:52:22 +0900

    dbに停留所を追加”

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 	"practice-colly/domain/model"
 	"practice-colly/infra"
 	"regexp"
+	"strconv"
 	"strings"
+
 	"github.com/gocolly/colly"
 	"github.com/labstack/echo"
 )
@@ -26,6 +28,8 @@ func main() {
 	e.Debug = true
 	e.Logger.Fatal(e.Start(":" + port))
 
+	// dbcreate()
+
 }
 
 
@@ -35,18 +39,18 @@ func Routing() {
 		return c.HTML(http.StatusOK, "<h1>Busdes! Clean Architecture API</h1>")
 	})
 
-	e.GET("/timetable",func(c echo.Context) error{
+	// e.GET("/timetable",func(c echo.Context) error{
 		
-		busstop := "西ノ京円町《ＪＲ円町駅》"
-		destination := "立命館大学行き"
-		busstoptourlCtrl := controller.BusstoToUrlController{}
-		url,err := busstoptourlCtrl.FindURL(busstop,destination)
-		if err != nil {
-		}
-		timetablecontroller := controller.TimetableController{}
-		timetable := timetablecontroller.FindTimetable(url)
-		return c.JSON(http.StatusOK,timetable)
-	})	
+	// 	busstop := "西ノ京円町《ＪＲ円町駅》"
+	// 	destination := "立命館大学行き"
+	// 	busstoptourlCtrl := controller.BusstoToUrlController{}
+	// 	url,err := busstoptourlCtrl.FindURL(busstop,destination)
+	// 	if err != nil {
+	// 	}
+	// 	timetablecontroller := controller.TimetableController{}
+	// 	timetable := timetablecontroller.FindTimetable(url)
+	// 	return c.JSON(http.StatusOK,timetable)
+	// })	
 	e.GET("/timetable",func(c echo.Context) error{
 		
 		busstop := c.QueryParam("fr")
@@ -79,6 +83,7 @@ func Routing() {
 		timetable := timetablecontroller.FindTimetable(url)
 		return c.JSON(http.StatusOK,timetable)
 	})
+
 }
 
 
@@ -198,68 +203,68 @@ func getTimeTable(scrapedata []string,via string,busstop string) {
 }
 
 
-// func dbcreate(){
-// 	db := infra.GetDB()
-// 	for i:=3300;i<999999;i++{
-// 		index := strconv.Itoa(i)
-// 		length := len(index)
-// 		idx := ""
+func dbcreate(){
+	db := infra.GetDB()
+	for i:=612211;i<700000;i++{
+		index := strconv.Itoa(i)
+		length := len(index)
+		idx := ""
 
-// 		switch(length){
-// 		case 1:
-// 			idx = "00000" + index
-// 		case 2:
-// 			idx = "0000"  + index
-// 		case 3:
-// 			idx = "000"   + index
-// 		case 4:
-// 			idx = "00"    + index
-// 		case 5:
-// 			idx = "0"     + index
-// 		case 6:
-// 			idx = index
-// 		}
-// 		busname,busstop,destination,url := getViaandBusstops(idx)
-// 		if len(busname) != 0{
-// 			db.Create(&model.BusstopUrl{Busstop: busstop,Busname: busname,Destination: destination,URL: url})
-// 		}
+		switch(length){
+		case 1:
+			idx = "000000" + index
+		case 2:
+			idx = "00000"  + index
+		case 3:
+			idx = "0000"   + index
+		case 4:
+			idx = "000"    + index
+		case 5:
+			idx = "00"     + index
+		case 6:
+			idx = "0"      + index
+		}
+		busname,busstop,destination,url := getViaandBusstops(idx)
+		if len(busname) != 0{
+			db.Create(&model.BusstopUrl{Busstop: busstop,Busname: busname,Destination: destination,URL: url})
+		}
 		
-// 	}
+	}
 
-// }
+}
 
 
 
-// func getViaandBusstops(index string) (busname string, busstop string, destination string, url string) {
-// 	c := colly.NewCollector()
+func getViaandBusstops(index string) (busname string, busstop string, destination string, url string) {
+	c := colly.NewCollector()
 
-// 	Busname := ""
-// 	Busstop := ""
-// 	Destination := ""
-// 	URL := "https://www2.city.kyoto.lg.jp/kotsu/busdia/hyperdia/" + index + ".htm"
+	Busname := ""
+	Busstop := ""
+	Destination := ""
+	URL := "https://www2.city.kyoto.lg.jp/kotsu/busdia/hyperdia/" + index + ".htm"
 
-// 	// Extract title element
-// 	c.OnHTML("h1", func(e *colly.HTMLElement) {
-// 		rep := regexp.MustCompile(` |　|\[|\]`)
-// 		e.Text = strings.Replace(e.Text, "-", "", -1)
-// 		result := rep.Split(e.Text, -1)
+	// Extract title element
+	c.OnHTML("h1", func(e *colly.HTMLElement) {
+		rep := regexp.MustCompile(` |　|\[|\]`)
+		e.Text = strings.Replace(e.Text, "-", "", -1)
+		result := rep.Split(e.Text, -1)
 
-// 		for i:=0;i<len(result);i++ {
-// 			if (strings.Contains(result[i], "行き")){
-// 				Destination = result[i]
-// 			}
-// 		}
+		for i:=0;i<len(result);i++ {
+			if (strings.Contains(result[i], "行き")){
+				Destination = result[i]
+			}
+		}
 
-// 		Busname = result[2]
-// 		Busstop = result[0]
+		Busname = result[2]
+		Busstop = result[0]
 
-// 		fmt.Println("駅名 : ", Busstop, "\nバス名 : ", Busname, "\n行き先 : ", Destination)
-// 	})
-// 	// Before making a request print "Visiting URL: https://XXX"
-// 	c.OnRequest(func(r *colly.Request) {
-// 		// fmt.Println("Visiting URL:", r.URL.String())
-// 	})
-// 	c.Visit(URL)
+		fmt.Println("駅名 : ", Busstop, "\nバス名 : ", Busname, "\n行き先 : ", Destination,"\nurl : ",URL)
+	})
+	// Before making a request print "Visiting URL: https://XXX"
+	c.OnRequest(func(r *colly.Request) {
+		// fmt.Println("Visiting URL:", r.URL.String())
+	})
+	c.Visit(URL)
 
-// 	return Busname,Busstop,Destination,URL
-// }
+	return Busname,Busstop,Destination,URL
+}
