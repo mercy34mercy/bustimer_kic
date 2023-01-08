299a0b26988a9b4645e23343a690b5c240265a40
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2023-01-09 01:06:31 +0900

    キャッシュ化の定期実行

--- infra/localcache/createTimetableCache.go
+++ infra/localcache/createTimetableCache.go
@@ -13,44 +13,44 @@ var (
 	c *cache.Cache
 )
 
-func Init() *cache.Cache{
+func Init() *cache.Cache {
 	c = cache.New(12*time.Hour, 24*time.Hour)
 	return c
 }
 
-func GetGoChache() *cache.Cache{
+func GetGoChache() *cache.Cache {
 	return c
 }
 
-
-
 func CreateTimetableCache() {
-	for _,busname := range config.BusNameList{
-	fmt.Printf(busname)
-	//バスの名前から、そのバスの停留所と行き先のセットを取得
-	busstoplistCtrl := controller.FindBusstopListController{}
-	busstoplist, err := busstoplistCtrl.FindBusstopList(busname)
-	if err != nil {
-	}
-
-	for _,businfo:= range busstoplist {
-		fmt.Printf(businfo.Busstop,businfo.Destination)
-		//停留所と行き先のセットからURLを取得
-		busstoptourlCtrl := controller.BusstoToUrlController{}
-		url, err := busstoptourlCtrl.FindURL(businfo.Busstop, businfo.Destination)
+	for _, busname := range config.BusNameList {
+		fmt.Printf(busname)
+		//バスの名前から、そのバスの停留所と行き先のセットを取得
+		busstoplistCtrl := controller.FindBusstopListController{}
+		busstoplist, err := busstoplistCtrl.FindBusstopList(busname)
 		if err != nil {
 		}
+		for {
+			for _, businfo := range busstoplist {
+				fmt.Printf(businfo.Busstop, businfo.Destination)
+				//停留所と行き先のセットからURLを取得
+				busstoptourlCtrl := controller.BusstoToUrlController{}
+				url, err := busstoptourlCtrl.FindURL(businfo.Busstop, businfo.Destination)
+				if err != nil {
+				}
 
-		//URLから時刻表を取得
-		timetablecontroller := controller.TimetableController{}
-		timetable := timetablecontroller.FindTimetable(url)
+				//URLから時刻表を取得
+				timetablecontroller := controller.TimetableController{}
+				timetable := timetablecontroller.FindTimetable(url)
 
-		c.Set(businfo.Busstop+businfo.Destination, timetable, cache.DefaultExpiration)
+				c.Set(businfo.Busstop+businfo.Destination, timetable, cache.DefaultExpiration)
+			}
+			fmt.Println("キャッシュ化完了")
+			time.Sleep(config.TimeTableCacheUpdateDuration)
+		}
+
 	}
 
-	}
-
-	fmt.Println("キャッシュ化完了")
 
 
 }
