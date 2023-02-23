161fef54d6cbece6f9fe01bccb94fb8951758afa
mercy34mercy <makun@DESKTOP.localdomain>
2023-02-23 12:44:58 +0900

    change dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,9 @@
-# 2020/10/14最新versionを取得
 FROM golang:1.19.3-buster
-# アップデートとgitのインストール！！
-# RUN apt update && apt upgrade
-# appディレクトリの作成
+
 RUN mkdir /go/src/app
-# ワーキングディレクトリの設定
+
 WORKDIR /go/src/app
-# ホストのファイルをコンテナの作業ディレクトリに移行
+
 ADD . /go/src/app
 
 CMD [ "go","run","./app/main.go" ]
--- domain/model/busstopUrl.go
+++ domain/model/busstopUrl.go
@@ -1,9 +1,9 @@
 package model
 
-type BusstopUrl struct{
-	ID int `gorm:"AUTO_INCREMENT"`
-	Busstop string `gorm:"type:varchar(255)"`
-	Busname string `gorm:"type:varchar(255)"`
-	Destination string `gorm:"type:varchar(255)"`
-	URL string `gorm:"type:varchar(255);unique"`
-}
+// type BusstopUrl struct{
+// 	ID int `gorm:"AUTO_INCREMENT"`
+// 	Busstop string `gorm:"type:varchar(255)"`
+// 	Busname string `gorm:"type:varchar(255)"`
+// 	Destination string `gorm:"type:varchar(255)"`
+// 	URL string `gorm:"type:varchar(255);unique"`
+// }
