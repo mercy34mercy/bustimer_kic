7b631689ea257ec2271219d1f40c954ad104f123
mercy34mercy <etukobamasatyan@gmail.com>
2023-04-03 00:15:50 +0900

    todayエンドポイントを実質的にはいし

--- bustimer/controller/timetableTodayController.go
+++ bustimer/controller/timetableTodayController.go
@@ -1,8 +1,6 @@
 package controller
 
 import (
-	"time"
-
 	"github.com/mercy34mercy/bustimer_kic/bustimer/domain/model"
 	repositoryImpl "github.com/mercy34mercy/bustimer_kic/bustimer/infra/repositoryImpl"
 	"github.com/mercy34mercy/bustimer_kic/bustimer/usecase"
@@ -14,19 +12,20 @@ func (ctrl TimetableTodayController) Get(busstop []string, destination []string)
 	busstoptotimetableRepository := repositoryImpl.NewBusstopToUrlRepositoryImpl()
 	timetable, err := usecase.NewGetUrlFromBusstopUseCaseImpl(busstop, destination, busstoptotimetableRepository).FindURLFromBusstop()
 
-	day := time.Now().Weekday()
-	if day == time.Saturday || day == time.Sunday {
-		for _, v := range timetable.TimeTable {
-			for i := 5; i <= 24; i++ {
-				v.Weekdays[i] = make([]model.OneBusTime, 0)
-			}
-		}
-	} else {
-		for _, v := range timetable.TimeTable {
-			for i := 5; i <= 24; i++ {
-				v.Holidays[i] = make([]model.OneBusTime, 0)
-			}
-		}
-	}
+	//day := time.Now().Weekday()
+	//
+	//if day == time.Saturday || day == time.Sunday {
+	//	for _, v := range timetable.TimeTable {
+	//		for i := 5; i <= 24; i++ {
+	//			v.Weekdays[i] = make([]model.OneBusTime, 0)
+	//		}
+	//	}
+	//} else {
+	//	for _, v := range timetable.TimeTable {
+	//		for i := 5; i <= 24; i++ {
+	//			v.Holidays[i] = make([]model.OneBusTime, 0)
+	//		}
+	//	}
+	//}
 	return timetable, err
 }
